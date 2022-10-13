45cfd0dcceaea3998c06e2ec6431d2dccdfdfce7
Randy Asuncion <ranasunc@gmail.com>
2022-10-13 04:21:21 -0700

    Create AddList component

---
+++ src/components/AddList.tsx
@@ -0,0 +1,11 @@
+import './AddList.css';
+
+const AddList = () => {
+	return (
+		<div className="add-list">
+			<div className="title editable">Add another list</div>
+		</div>
+	);
+};
+
+export default AddList;
