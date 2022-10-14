c8db55f875a0e7b44680894ec5e982ddf016bdcd
Randy Asuncion <ranasunc@gmail.com>
2022-10-14 00:52:33 -0700

    Add container

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -21,13 +21,15 @@ const List: React.FC<ListType> = ({ title, cards }) => {
 
 	return (
 		<div className="list">
-			<div className="title removable editable">{title}</div>
-			<div className="content" ref={ref}>
-				{cards.map((content) => (
-					<Card content={content} />
-				))}
+			<div className="container">
+				<div className="title removable editable">{title}</div>
+				<div className="content" ref={ref}>
+					{cards.map((content) => (
+						<Card content={content} />
+					))}
+				</div>
+				<div className="add-card editable">Add another card</div>
 			</div>
-			<div className="add-card editable">Add another card</div>
 		</div>
 	);
 };
